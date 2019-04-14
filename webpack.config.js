f4be7fcb953442a4e7d92a984a6b03f4b8f25669
Minh Trí <MinhTri@TmTzMinhTri.local>
2019-04-15 02:48:04 +0700

    setup project reactjs with webpack

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require("path")
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve('dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: "style-loader" },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            import: true
+                        }
+                    }
+                ],
+            },
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+    })]
+};
